54fdce4e1f7fbf9da4e207256d3f58e9ca5ff324
Paul-Willy Jean <paul-willy.jean@geninov.com>
2015-04-22 19:04:01 -0400

    Ajout de fichiers de context pour les services de dspace
    - Exclut jspui puisqu'il ne compile pas avec tomcat8

--- dspace/entrypoint.sh
+++ dspace/entrypoint.sh
@@ -5,7 +5,22 @@ if [ "${1}" = "run" ];then
     set -- catalina.sh "$@"
 fi
 
-ln -sf /dspace/webapps/* $CATALINA_HOME/webapps/
+mkdir -p $CATALINA_HOME/conf/Catalina/localhost
+
+for webapp in $(ls /dspace/webapps/); do
+    # Exclue jspui parce qu'il n'est pas compilable avec tomcat8
+    if [ "$webapp" != "jspui" ]; then
+        {
+            echo "<?xml version='1.0'?>"
+            echo "<Context"
+            echo docBase=\"/dspace/webapps/$webapp\"
+            echo 'reloadable="true"'
+            echo 'cachingAllowed="false"/>'
+        } > $CATALINA_HOME/conf/Catalina/localhost/$webapp.xml
+    fi
+done
+
+cp $CATALINA_HOME/conf/Catalina/localhost/{xmlui,ROOT}.xml
 sed -i "s/localhost:5432/db:5432/" /dspace/config/dspace.cfg
 
 exec "$@"
